71f0f4bd55f14f6237b93f170d1a892e5c813f48
Three <Three@EI>
2025-06-14 17:50:09 +0800

    Add project files.

---
+++ Activity 1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace FunctionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("Activity 1");
+            Console.WriteLine("C# Function Add, Subtract, Multiply, and Divide");
+
+            Console.Write("Enter first number: ");
+            int fn = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Second number: ");
+            int sn = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("");
+
+            Console.WriteLine($"Add: {Add(fn, sn)}");
+            Console.WriteLine($"Subtract: {Sub(fn, sn)}");
+            Console.WriteLine($"Multiply: {Mul(fn, sn)}");
+            Console.WriteLine($"Divide: {Div(fn, sn)}");
+        }
+
+        static int Add(int fn, int sn) => fn + sn;
+        static int Sub(int fn, int sn) => fn - sn;
+        static int Mul(int fn, int sn) => fn * sn;
+        static double Div(int fn, int sn) => sn != 0 ? (double)fn / sn : 0;
+    }
+}
+
+
